5cf8b0181ed7315970290d00956e4801b32a05ed
rimasultana <rimasultana20225@gmail.com>
2024-03-16 07:04:59 +0600

    getter

---
+++ index.js
@@ -0,0 +1,10 @@
+const person ={
+    firstName : "Rima",
+    lastName : "Sultana",
+    language : "en",
+    get lang(){
+        return this.language;
+    }
+};
+
+console.log(person.language);
